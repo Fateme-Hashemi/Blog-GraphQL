40b3d5be49c23b1aa619fe2f953371b94edd226b
elahe hs <elahe.hs481@gmail.com>
2024-05-25 21:34:31 +0330

    initialize blog project with graphql and mui

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client'
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@mui/material';
+import theme from './mui/Theme';
+
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_GRAGHCMS_URI,
+  cache: new InMemoryCache(),
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+   <ThemeProvider theme={theme}>
+        <App />
+        </ThemeProvider>
+  </ApolloProvider>
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
---
+++ src/mui/Theme.js
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    typography: {
+        fontFamily: "Oswald",
+        fontWeightLight:300,
+        fontWeightRegular: 400,
+        fontWeightMedium: 500,
+        fontWeightBold: 700,
+    },
+})
+
+export default theme;
